dc361d26548f6c60a7b00a8ea7ad79c6ee485be0
banachtech <balaji@banach.tech>
2023-05-13 07:31:31 +0800

    remove Flux based test

--- src/DeepLib.jl
+++ src/DeepLib.jl
@@ -1,10 +1,6 @@
 module DeepLib
 
-using Reexport
-
-@reexport using Flux
 using Random
-using LinearAlgebra
 include("importance.jl")
 
 export importance
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,15 @@
-using DeepLib
-using Test
+using DeepLib, Test
+using Random
 
 
 @testset "DeepLib.jl" begin
     # fake data
     X = Float32.(randn(4,100))
     Y = X[2,:].^2 - 3.0*X[3,:].^3
-    Y = reshape(Y,1,:)
-    # define a simple MLP
-    model = Flux.Chain(Dense(4,2,tanh),Dense(2,2,tanh),Dense(2,1))
+    # simple model
+    model(x) = vec(sum(x,dims=1))
     # mse loss function
-    lossfn(m,x;target=nothing) = Flux.mse(m(x),target)
+    lossfn(m,x;target=nothing) = sum(abs2, m(x) .- target)
     res = importance(model,lossfn,X,target=Y)
     @test length(res) == 4
 end
